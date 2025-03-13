9bb3992ed366b690a2ccee00fa013a4edc127f1a
michellejtan <michelle0223@gmail.com>
2025-03-13 01:35:32 -0700

    wip: redirect to foods index and render template

--- controllers/foods.js
+++ controllers/foods.js
@@ -3,5 +3,14 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+// GET /users/:userId/foods
+router.get('/', (req, res) => {
+    try {
+        res.render('foods/index.ejs');
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
 
 module.exports = router;
