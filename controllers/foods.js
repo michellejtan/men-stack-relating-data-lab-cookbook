08094f7832102b8c409c940e954f4b566ab64139
michellejtan <michelle0223@gmail.com>
2025-03-14 07:14:56 -0700

    wip: complete delete functionality (route & button)

--- controllers/foods.js
+++ controllers/foods.js
@@ -7,7 +7,7 @@ const User = require('../models/user.js');
 router.get('/', async (req, res) => {
     try {
         const currentUser = await User.findById(req.session.user._id);
-         // Assign pantry items to res.locals
+        // Assign pantry items to res.locals
          res.locals.pantry = currentUser.pantry;
         // pantry items are now available in the view
          res.render('foods/index.ejs');
@@ -28,18 +28,26 @@ router.get('/new', async (req, res) => {
 //POST /users/:userID/foods
 router.post('/', async (req, res) => {
     try {
-      const currentUser = await User.findById(req.session.user._id);
-      // Push req.body (the new form data object) to the
-      // applications array of the current user
-      currentUser.pantry.push(req.body);
-      // Save changes to the user
-      await currentUser.save();
-      // Redirect back to the food index view
-      res.redirect(`/users/${currentUser._id}/foods`);
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.push(req.body);
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
     } catch (error) {
-      // If any errors, log them and redirect back home
-      console.log(error);
-      res.redirect('/');
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// DELETE /users/:userId/foods/:itemId
+router.delete('/:itemId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.pantry.id(req.params.itemId).deleteOne();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
     }
 });
 
