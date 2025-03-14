948fd6ed712839a4eaee22a22bda90eb41ee2576
michellejtan <michelle0223@gmail.com>
2025-03-13 20:54:30 -0700

    wip: update food index route to send pantry items to index page via res.locals & complete create functionality

--- controllers/foods.js
+++ controllers/foods.js
@@ -4,9 +4,16 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 // GET /users/:userId/foods
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        res.render('foods/index.ejs');
+        const currentUser = await User.findById(req.session.user._id);
+         // Assign pantry items to res.locals
+         res.locals.pantry = currentUser.pantry;
+        // pantry items are now available in the view
+         res.render('foods/index.ejs');
+        // res.render('foods/index.ejs', {
+        //     pantry: currentUser.pantry,
+        // });
     } catch (error) {
         console.log(error);
         res.redirect('/');
@@ -16,6 +23,24 @@ router.get('/', (req, res) => {
 // GET /users/:userId/foods/new
 router.get('/new', async (req, res) => {
     res.render('foods/new.ejs');
-  });
+});
+
+//POST /users/:userID/foods
+router.post('/', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      // Push req.body (the new form data object) to the
+      // applications array of the current user
+      currentUser.pantry.push(req.body);
+      // Save changes to the user
+      await currentUser.save();
+      // Redirect back to the food index view
+      res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/');
+    }
+});
 
 module.exports = router;
