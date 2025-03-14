e76a27a751a891981190b45beccc06c9f9e98ed1
michellejtan <michelle0223@gmail.com>
2025-03-14 08:20:50 -0700

    complete edit template and edit route

--- controllers/foods.js
+++ controllers/foods.js
@@ -8,9 +8,9 @@ router.get('/', async (req, res) => {
     try {
         const currentUser = await User.findById(req.session.user._id);
         // Assign pantry items to res.locals
-         res.locals.pantry = currentUser.pantry;
+        res.locals.pantry = currentUser.pantry;
         // pantry items are now available in the view
-         res.render('foods/index.ejs');
+        res.render('foods/index.ejs');
         // res.render('foods/index.ejs', {
         //     pantry: currentUser.pantry,
         // });
@@ -45,6 +45,22 @@ router.delete('/:itemId', async (req, res) => {
         currentUser.pantry.id(req.params.itemId).deleteOne();
         await currentUser.save();
         res.redirect(`/users/${currentUser._id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+// GET /users/:userId/foods/:itemId/edit
+router.get('/:itemId/edit', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        // const foodItem = currentUser.pantry.id(req.params.itemId);
+        res.locals.foodItem = currentUser.pantry.id(req.params.itemId);
+        res.render('foods/edit.ejs');
+        // res.render('foods/edit.ejs', {
+        //     foodItem: foodItem,
+        // });
     } catch (error) {
         console.log(error);
         res.redirect('/');
